d21e1ba452010c2f1df9d7c2e637593b3d9a59e4
sparda-65 <chahboune.abdellah@gmail.com>
2019-12-01 00:58:49 +0100

    add title as parametre and page one and two

--- lib/main.dart
+++ lib/main.dart
@@ -5,29 +5,49 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final String _appBarTitle="Startup Name Generator";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Startup Name Generator',
-        home: RandomWords(),
+        home: RandomWords(_appBarTitle),
     );
   }
 }
 
-void openPage(BuildContext context) {
+void pageOne(BuildContext context) {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final SnackBar snackBar = const SnackBar(content: Text('Showing Snackbar'));
   Navigator.push(context, MaterialPageRoute(
     builder: (BuildContext context) {
       return Scaffold(
+        key: scaffoldKey,
         appBar: AppBar(
           title: const Text(
               'Next page',
-              style: TextStyle(fontSize: 24,fontFamily: 'IndieFlower')
+              style: TextStyle(fontSize: 24,fontFamily: 'RobotoMono')
         ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.add_alert),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                scaffoldKey.currentState.showSnackBar(snackBar);
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.navigate_next),
+              tooltip: 'Next page One',
+              onPressed: () {
+                pageTwo(context);
+              },
+            ),
+          ],
         ),
         body: const Center(
           child: Text(
-            'This is the next page',
-            style: TextStyle(fontSize: 24,fontFamily: 'IndieFlower'),
+            'This is Page One',
+            style: TextStyle(fontSize: 24,fontFamily: 'Raleway'),
           ),
         ),
       );
@@ -35,7 +55,44 @@ void openPage(BuildContext context) {
   ));
 }
 
-class RandomWordsState extends State<RandomWords> {
+void pageTwo(BuildContext context) {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final SnackBar snackBar = const SnackBar(content: Text('Showing Snackbar'));
+  Navigator.push(context, MaterialPageRoute(
+    builder: (BuildContext context) {
+      return Scaffold(
+        key: scaffoldKey,
+        appBar: AppBar(
+          title: const Text(
+              'Next page',
+              style: TextStyle(fontSize: 24,fontFamily: 'RobotoMono')
+          ),
+          actions:<Widget>[
+            IconButton(
+              icon: const Icon(Icons.add_alert),
+              tooltip: 'Show Snackbar',
+              onPressed: () {
+                scaffoldKey.currentState.showSnackBar(snackBar);
+              },
+            ),
+          ]
+        ),
+        body: const Center(
+          child: Text(
+            'This is Page Two',
+            style: TextStyle(fontSize: 24,fontFamily: 'Raleway'),
+          ),
+        ),
+      );
+    },
+  ));
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final String _appBarTitle;
+
+  _RandomWordsState(this._appBarTitle);
+
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0, fontFamily: 'IndieFlower');
 
@@ -70,7 +127,7 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       key: scaffoldKey,
       appBar: AppBar(
-        title: const Text('Startup Name Generator'),
+        title: Text(this._appBarTitle),
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.add_alert),
@@ -83,7 +140,7 @@ class RandomWordsState extends State<RandomWords> {
             icon: const Icon(Icons.navigate_next),
             tooltip: 'Next page',
             onPressed: () {
-              openPage(context);
+              pageOne(context);
             },
           ),
         ],
@@ -95,7 +152,11 @@ class RandomWordsState extends State<RandomWords> {
 }
 
 class RandomWords extends StatefulWidget {
+  final String _appBarTitle;
+
+  RandomWords(this._appBarTitle, { Key key }) : super(key: key);
+
   @override
-  RandomWordsState createState() => RandomWordsState();
+  _RandomWordsState createState() => _RandomWordsState(_appBarTitle);
 }
 
